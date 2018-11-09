731ae1c8b6b2e532bf270be674b94f8fd2223d6e
Fernando G <kev9504@gmail.com>
2018-11-09 19:37:18 +0000

    tested upgrading packages to resolve hook isse. Fix still pending

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -241,8 +241,8 @@ module.exports = {
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In production, it will be an empty string unless you specify "homepage"
     // in `package.json`, in which case it will be the pathname of that URL.
-    new InterpolateHtmlPlugin(env.raw),
     // Generates an `index.html` file with the <script> injected.
+    new InterpolateHtmlPlugin(env.raw),
     new HtmlWebpackPlugin({
       inject: true,
       template: paths.appHtml,
@@ -259,6 +259,7 @@ module.exports = {
         minifyURLs: true,
       },
     }),
+    
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
     // It is absolutely essential that NODE_ENV was set to production here.
